122c0bea9f656dad099e375531a4c505f59d5f1a
Zdravko Kolev <zkolev@infragistics.com>
2024-12-16 19:19:53 +0200

    Scheduler enhancements

--- src/app/scheduler/scheduler.component.ts
+++ src/app/scheduler/scheduler.component.ts
@@ -8,4 +8,17 @@ import { IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxOverlayOutletDirecti
   styleUrls: ['./scheduler.component.scss']
 })
 export class SchedulerComponent {
+
+  bookAppointment(event: Event) {
+    event.preventDefault();
+    const form = event.target as HTMLFormElement;
+    const date = (form.querySelector('#date') as HTMLInputElement).value;
+    const time = (form.querySelector('#time') as HTMLInputElement).value;
+    
+    // New code to send an email
+    //const mailtoLink = `mailto:dirty.garage23@gmail.com?subject=Искам час за полиране на фарове&body=Искам да запазя час за ${date} в ${time} часа.`;
+    const mailtoLink = `mailto:dirty.garage23@gmail.com?subject=Искам час за полиране на фарове&body=Искам да запазя час за 2024-12-20 в 14:00 часа.%0A%0AМожете да добавите срещата в Google Calendar чрез следния линк:%0Ahttps%3A%2F%2Fcalendar.google.com%2Fcalendar%2Frender%3Faction%3DTEMPLATE%26text%3DЗапазване%2520на%2520час%2520за%2520полиране%26dates%3D20241220T140000%2F20241220T150000%26details%3DПолиране%2520на%2520фарове%26location%3DDirty%2520Garage `;
+    
+    window.location.href = mailtoLink;
+  }
 }
