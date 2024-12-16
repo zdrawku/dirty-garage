7a5c92c23e598ffac2592bb59ae8ec50bbd32c8f
Zdravko Kolev <zdrawko.kolew@gmail.com>
2024-12-16 18:38:13 +0200

    Dirty Garage updates

---
+++ src/app/scheduler/scheduler.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective } from 'igniteui-angular';
+
+@Component({
+  selector: 'app-scheduler',
+  imports: [IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective],
+  templateUrl: './scheduler.component.html',
+  styleUrls: ['./scheduler.component.scss']
+})
+export class SchedulerComponent {
+}
