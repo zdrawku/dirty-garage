f57193b6cc5fc1e97bafb4bac5704660e530c5c0
Zdravko Kolev <zkolev@infragistics.com>
2024-12-17 14:32:41 +0200

    Final touches

--- src/app/scheduler/scheduler.component.ts
+++ src/app/scheduler/scheduler.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective } from 'igniteui-angular';
+import { IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxRippleDirective } from 'igniteui-angular';
 
 @Component({
   selector: 'app-scheduler',
-  imports: [IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxOverlayOutletDirective, IgxRippleDirective, IgxToggleActionDirective, IgxToggleDirective],
+  imports: [IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective, IgxRippleDirective],
   templateUrl: './scheduler.component.html',
   styleUrls: ['./scheduler.component.scss']
 })
@@ -15,9 +15,7 @@ export class SchedulerComponent {
     const date = (form.querySelector('#date') as HTMLInputElement).value;
     const time = (form.querySelector('#time') as HTMLInputElement).value;
     
-    // New code to send an email
-    //const mailtoLink = `mailto:dirty.garage23@gmail.com?subject=Искам час за полиране на фарове&body=Искам да запазя час за ${date} в ${time} часа.`;
-    const mailtoLink = `mailto:dirty.garage23@gmail.com?subject=Искам час за полиране на фарове&body=Искам да запазя час за 2024-12-20 в 14:00 часа.%0A%0AМожете да добавите срещата в Google Calendar чрез следния линк:%0Ahttps%3A%2F%2Fcalendar.google.com%2Fcalendar%2Frender%3Faction%3DTEMPLATE%26text%3DЗапазване%2520на%2520час%2520за%2520полиране%26dates%3D20241220T140000%2F20241220T150000%26details%3DПолиране%2520на%2520фарове%26location%3DDirty%2520Garage `;
+    const mailtoLink = `mailto:dirty.garage23@gmail.com?subject=Искам час за полиране на фарове&body=Искам да запазя час за ${date} в ${time} часа.%0A%0AМожете да добавите срещата в Google Calendar чрез следния линк:%0Ahttps%3A%2F%2Fcalendar.google.com%2Fcalendar%2Frender%3Faction%3DTEMPLATE%26text%3DЗапазване%2520на%2520час%2520за%2520полиране%26dates%3D20241220T140000%2F20241220T150000%26details%3DПолиране%2520на%2520фарове%26location%3DDirty%2520Garage `;
     
     window.location.href = mailtoLink;
   }
