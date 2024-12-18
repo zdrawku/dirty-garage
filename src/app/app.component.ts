8e2a14ae2b71ab8acb255f056bf271d886fbc84a
Zdravko Kolev <zkolev@infragistics.com>
2024-12-18 10:37:24 +0200

    Action buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,4 +12,15 @@ export class AppComponent {
   public iconButtonVisible: boolean = false;
   public iconButtonVisible1: boolean = false;
   public iconButtonVisible2: boolean = false;
+  phoneNumber: string = '+359898443755';
+
+  openGoogleMaps(): void {
+    const place = 'София, ж.к. Младост 2 272Б';
+    const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(place)}`;
+    window.open(googleMapsUrl, '_blank'); // Open in a new tab
+  }
+
+  openPhoneNumber(): void {
+    window.location.href = `tel:${this.phoneNumber}`;
+  }
 }
