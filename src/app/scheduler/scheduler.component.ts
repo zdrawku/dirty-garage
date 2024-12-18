4659bd43df0b7362f55425170d56a22563a3d07d
zkolev <zkolev@infragistics.com>
2024-12-18 14:40:21 +0200

    Update mail template

--- src/app/scheduler/scheduler.component.ts
+++ src/app/scheduler/scheduler.component.ts
@@ -15,10 +15,12 @@ export class SchedulerComponent {
     const date = (form.querySelector('#date') as HTMLInputElement).value;
     const time = (form.querySelector('#time') as HTMLInputElement).value;
     const mail = (form.querySelector('#mail') as HTMLInputElement).value;
+    const phone = (form.querySelector('#phone') as HTMLInputElement).value;
 
-    const subject = encodeURIComponent(`Искам час за полиране на фарове от ${mail}`);
-    const body = encodeURIComponent(`Искам да запазя час за ${date} в ${time} часа.\n\nМожете да добавите срещата в Google Calendar чрез следния линк:\nhttps://calendar.google.com/calendar/render?action=TEMPLATE&text=Запазване на час за полиране от ${mail}&dates=${date.replace(/-/g, '')}T${time.replace(':', '')}00/${date.replace(/-/g, '')}T${(parseInt(time.split(':')[0]) + 1).toString().padStart(2, '0')}${time.split(':')[1]}00&details=Полиране на фарове&location=Dirty Garage`);
+    const subject = encodeURIComponent(`Искам час за полиране на фарове от ${mail || phone}`);
+    const calendarLink = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(`Запазване на час за полиране от ${mail || phone}`)}&dates=${date.replace(/-/g, '')}T${time.replace(':', '')}00/${date.replace(/-/g, '')}T${(parseInt(time.split(':')[0]) + 1).toString().padStart(2, '0')}${time.split(':')[1]}00&details=${encodeURIComponent(`Полиране на фарове\nИмейл: ${mail}\nТелефон: ${phone}`)}&location=${encodeURIComponent('Dirty Garage')}`;
+    const body = encodeURIComponent(`Искам да запазя час за ${date} в ${time} часа.\n\nМожете да добавите срещата в Google Calendar чрез следния линк:\n${calendarLink}\n\nТелефон: ${phone}\nИмейл: ${mail}`);
     const mailtoLink = `mailto:dirty.garage23@gmail.com?subject=${subject}&body=${body}`;
     window.location.href = mailtoLink;
-  }
+}
 }
